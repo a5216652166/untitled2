e23b782ed5a6d81b628a96254da2a082ee69bf2a
gaoyan <373251686@qq.com>
2017-11-26 19:20:58 +0800

    改造

--- t.py
+++ t.py
@@ -60,5 +60,17 @@ def dd(ta,t,T):
     return dd(ta,t,T) if i<T else ta
 if __name__ == '__main__':
     # pprint(calsum(2,6))
-    zzz=dd(0,6,2).subs({a:1.0,m:0.07,n:0.06}).evalf(30)
+    t_a=1.0
+    t_m=0.07
+    t_n=0.06
+    t_0=0
+    t_mon=12
+    t_year=2
+    t_a,t_m,t_n,t_0,t_mon,t_year = float(raw_input('输入金额：')),\
+                                   float(raw_input('输入产品利率：')),\
+                                   float(raw_input('输入借款利率:')),\
+                                   float(raw_input('输入前置自有金额:')),\
+                                   float(raw_input('输入借款记息周期（x>1 and x<12 and 12.mod(x)=0）单位月:')),\
+                                   float(raw_input('输入产品周期单位年 便不能为负数:'))
+    zzz=dd(t_0,t_mon,t_year).subs({a:t_a,m:t_m,n:t_n}).evalf(30)
     pprint (zzz)
